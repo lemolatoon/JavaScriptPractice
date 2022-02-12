eacd63110344ab32abef74002e516805a7e94367
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-02-12 17:01:22 +0900

    Impl sample of how to draw pixcels on canvas

--- canvas.js
+++ canvas.js
@@ -1,5 +1,26 @@
+// Initialization
 let canvas = document.getElementById("board");
 let ctx = canvas.getContext("2d");
+const canvasWidth = canvas.clientWidth;
+const canvasHeight = canvas.clientHeight;
+// alert(canvasWidth + ", " + canvasHeight);
+ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+
+const imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+const pixels = imageData.data;
+
+for (let i = 0; i < canvasHeight; i++) {
+    for (let j = 0; j < canvasHeight; j++) {
+        for (let k = 0; k < 4; k++) {
+            // r g b a
+            if (k == 0 || k == 2) continue;
+            pixels[(i * canvasHeight + j) * 4 + k] = 0xff;
+        }
+    }
+}
+
+ctx.putImageData(imageData, 0, 0);
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50)
